2ba55ca6bf3881e08eed265101600eca1ef9d364
Choi Seung Ho <74850409+chltmdgh522@users.noreply.github.com>
2025-05-04 15:28:54 +0900

    [D3] Title: 부분 수열의 합, Time: 90 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -3,55 +3,34 @@ import java.util.*;
 
 public class Solution {
 
-    static int N;
-
-    static int K;
-
-    static int[] num;
-
-    static int result;
-
-    static int sum;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
-        for (int test = 1; test < T + 1; test++) {
+        for (int test = 1; test <= T; test++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            result = 0;
-            N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            int[] num = new int[N];
             st = new StringTokenizer(br.readLine());
-            num = new int[N];
             for (int i = 0; i < N; i++) {
                 num[i] = Integer.parseInt(st.nextToken());
             }
-            sum = 0;
-            solution(0);
-            System.out.println("#" + test + " " + result);
-        }
-    }
 
-    static void solution(int level) {
-        if (sum == K) {
-            result++;
-            return;
-        }
+            int[][] dp = new int[N + 1][K + 1];
+            dp[0][0] = 1;  // 아무것도 선택하지 않았을 때 합 0 하나 존재
 
-        if (level == N) {
-            return;
-        }
-
-
-        for (int i = level; i < N; i++) {
-            sum += num[i];
-            if (sum > K) {
-                sum -= num[i];
-                continue;
+            for (int i = 1; i <= N; i++) {
+                for (int j = 0; j <= K; j++) {
+                    dp[i][j] = dp[i - 1][j];  // i번째 숫자 안 쓴 경우
+                    if (j >= num[i - 1]) {
+                        dp[i][j] += dp[i - 1][j - num[i - 1]];  // i번째 숫자 쓴 경우
+                    }
+                }
             }
-            solution(i + 1);
-            sum -= num[i];
+
+            System.out.println("#" + test + " " + dp[N][K]);
         }
     }
 }
