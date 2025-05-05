f5719d1bc1379ba954fb6e1c412cbaca48f6b11f
Choi Seung Ho <74850409+chltmdgh522@users.noreply.github.com>
2025-05-05 17:33:37 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 98 ms, Memory: 32,884 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -2,55 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
     static int N;
-
     static char[] num;
-
     static String result;
-
     static int length;
 
+    static Set<String>[] visited; // 깊이에 따른 방문 상태 저장
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
-        for (int test = 1; test < T + 1; test++) {
+        for (int test = 1; test <= T; test++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String input = st.nextToken();
             N = Integer.parseInt(st.nextToken());
-            length = input.length();
 
             num = input.toCharArray();
-            result = "";
-            solution(0,0);
+            length = num.length;
+            result = "0";
+
+            visited = new HashSet[N + 1]; // 깊이마다 Set 초기화
+            for (int i = 0; i <= N; i++) {
+                visited[i] = new HashSet<>();
+            }
+
+            solution(0);
+
             System.out.println("#" + test + " " + result);
         }
     }
 
-    private static void solution(int depth, int count) {
+    private static void solution(int count) {
+        String current = new String(num);
+        if (visited[count].contains(current)) return;
+        visited[count].add(current);
+
         if (count == N) {
-            String current = new String(num);
-            if(current.compareTo(result) > 0){
+            if (current.compareTo(result) > 0) {
                 result = current;
             }
             return;
         }
 
-        for (int i = depth; i < length; i++) {
-            for (int j = i+1; j < length; j++) {
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j < length; j++) {
                 swap(i, j);
-                solution(i,count+1);
-                swap(i, j);
+                solution(count + 1);
+                swap(i, j); // backtracking
             }
         }
     }
 
-    private static void swap(int front, int back) {
-        char temp = 0;
-        temp = num[front];
-        num[front] = num[back];
-        num[back] = temp;
+    private static void swap(int i, int j) {
+        char tmp = num[i];
+        num[i] = num[j];
+        num[j] = tmp;
     }
 }
